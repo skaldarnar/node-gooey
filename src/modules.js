4660861b7f038b76318fe59ab66e6ee9a5e8569c
Tobias Nett <skaldarnar@googlemail.com>
2021-04-17 16:58:28 +0200

    feat: allow to increment a module version across the whole workspace

--- src/modules.js
+++ src/modules.js
@@ -1,10 +1,11 @@
 //@ts-check
 
+//@ts-check
 const fs = require("fs-extra");
+const semver = require("semver");
 const git = require("isomorphic-git");
 const { join, basename } = require("path");
 const chalk = require("chalk");
-const execa = require("execa");
 
 const simpleGit = require('simple-git');
 const gitw = simpleGit();
@@ -29,6 +30,52 @@ async function reset(dir) {
   return msg;
 }
 
+const MODULE = "module.txt";
+
+async function increment(module, level) {
+    const moduleInfoFile = join(module, MODULE);
+    const moduleInfo = await fs.readJSON(moduleInfoFile);
+
+    const currentVersion = moduleInfo.version;
+    if (level.startsWith("pre")) {
+      moduleInfo.version = semver.inc(currentVersion, level.substring(3)) + "-SNAPSHOT";
+    } else {
+      moduleInfo.version = semver.inc(currentVersion, level);
+    }
+
+    await fs.writeJSON(moduleInfoFile, moduleInfo, { spaces: 4 });
+    
+    return {
+      id: moduleInfo.id,
+      oldVersion: currentVersion,
+      newVersion: moduleInfo.version
+    }
+}
+
+async function updateDependency(module, dependency, version) {
+    const moduleInfoFile = join(module, MODULE);
+    const moduleInfo = await fs.readJSON(moduleInfoFile);
+
+    const entry = moduleInfo.dependencies.find(el => el.id === dependency);
+    if (entry) {
+      const currentVersion = entry.minVersion;
+      entry.minVersion = version
+
+      await fs.writeJSON(moduleInfoFile, moduleInfo, { spaces: 4 });
+
+      return {
+        id: moduleInfo.id,
+        oldVersion: currentVersion,
+        newVersion: version
+      }
+    }
+    return {
+      id: moduleInfo.id
+    }
+}
+
 module.exports = {
+  increment,
+  updateDependency,
   reset,
 };
